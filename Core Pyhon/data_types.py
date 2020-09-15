8293e8f86829441138ee495c9e599c2e8943063c
KT <46636573+kt-SoftwareQualityAssurance@users.noreply.github.com>
2020-09-15 18:04:13 -0400

    Data types, operators and new practice session

---
+++ Core Pyhon/data_types.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------------
+# Rules
+# no spaces
+# Use only ordinary leeters, numbers and uncerscores.
+# Need to start with letter or underscore.
+# -------------------------------------------------------
+
+# variable declaration 
+var_a = 5
+var_b = 10
+# or 
+var_a, var_b = 5,10
+
+# int and Float conversion
+temp_float_value = 500.888
+temp_int_calue = 333
+print(int(temp_float_value))
+print(float(temp_int_calue))
---
+++ Core Pyhon/operators.py
@@ -0,0 +1,20 @@
+# + addition
+# - Subtraction 
+# * Multiplication
+# / division
+# % Modulas
+# ** Exponentioation 
+# // Divides and rounds down to nearest integet
+
+
+# Assignment operators
+x = 2
+x += 1
+x -+1
+
+
+carrots = 24
+rabbits = 8
+crs_per_rab = carrots/rabbits
+rabbits = 12
+print(crs_per_rab)
